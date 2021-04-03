5d9b91bd95e1db4fe86bac91c0250ab6c721d892
KasTheDeveloper <kasra.shirazi.1234@gmail.com>
2021-04-03 20:23:47 +0430

    json,bson are added

--- Models/userEventData.go
+++ Models/userEventData.go
@@ -1,12 +1,12 @@
 package Models
 
 type UserEventData struct {
-	EventId string
-	UserEventStage string
-	//UserMetaData types.Array
-	Score  int
-	JoinTime int64
-	EndTime int64
-	StartTime int64
-	PreActiveTime int64
+	EventId string				`json:"eventId"`
+	UserEventStage string		`json:"userEventStage"`
+	//UserMetaData types.Array	`json:"marketName"`
+	Score  int					`json:"score"`
+	JoinTime int64				`json:"joinTime"`
+	EndTime int64				`json:"endTime"`
+	StartTime int64				`json:"startTime"`
+	PreActiveTime int64			`json:"preActiveTime"`
 }
