078f51d8c8bd1c19ac51b95c3d0ba2fee7777d7c
KasTheDeveloper <kasra.shirazi.1234@gmail.com>
2021-04-04 19:57:01 +0430

    Bad_Request

--- Utils/Errors/rest_errors.go
+++ Utils/Errors/rest_errors.go
@@ -1,8 +1,19 @@
 package Errors
+
+import "net/http"
+
 //TODO: CREATE REST ERRORS AND USE IT ANYWHERE!!
 
 type RestError struct {
 	Message string	`json:"message"`
 	Status 	int		`json:"code"`
 	Error 	string	`json:"error"`
+}
+
+func BadRequest(message string) *RestError {
+	return &RestError{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "Bad_Request",
+	}
 }
